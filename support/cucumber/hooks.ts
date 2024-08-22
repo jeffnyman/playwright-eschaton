a9dc6335ed4b5cbaa6d3eb378ae2fcdf2da01916
Jeff Nyman <jeffnyman@gmail.com>
2024-08-22 07:45:03 -0500

    Add ability to get screenshot.

--- support/cucumber/hooks.ts
+++ support/cucumber/hooks.ts
@@ -1,21 +1,41 @@
-import { Before, After } from "@cucumber/cucumber";
-import { chromium, Browser, Page } from "@playwright/test";
+import { Before, After, AfterAll } from "@cucumber/cucumber";
+import { chromium, Browser, BrowserContext } from "@playwright/test";
 import { pageFixture } from "./pageFixture";
 
 let browser: Browser;
-let page: Page;
+let context: BrowserContext;
 
 Before(async function (scenario) {
   if (!scenario.pickle.tags.some((tag) => tag.name === "@canary")) {
-    browser = await chromium.launch({ headless: false });
-    page = await browser.newPage();
+    // I'm putting this check here rather than using the BeforeAll
+    // hook. I'm doing that because the BeforeAll does not accept
+    // the `scenario` argument so I would not be able to check for
+    // the tag.
+    if (!browser) {
+      browser = await chromium.launch({ headless: false });
+    }
+    context = await browser.newContext();
+    const page = await context.newPage();
     pageFixture.page = page;
   }
 });
 
 After(async function (scenario) {
   if (!scenario.pickle.tags.some((tag) => tag.name === "@canary")) {
-    await page.close();
+    const img = await pageFixture.page.screenshot({
+      path: `./results/screenshots/${scenario.pickle.name}.png`,
+      type: "png",
+    });
+
+    this.attach(img, "image/png");
+
+    await pageFixture.page.close();
+    await context.close();
+  }
+});
+
+AfterAll(async () => {
+  if (browser) {
     await browser.close();
   }
 });
